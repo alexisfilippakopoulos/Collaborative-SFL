c4aa3ce4563757d8a3cd0d83fad542284e05aa0c
alex <alexisfilippakopoulos1@gmail.com>
2024-09-27 19:43:34 +0300

    Added dictionary structure

---
+++ create_subsets.py
@@ -0,0 +1,62 @@
+from torch.utils.data import Subset
+from torch import save
+from torchvision import datasets, transforms
+import os
+
+
+def get_data(transform, cifar_flag):
+    if cifar_flag:
+        train_data = datasets.CIFAR10(root='data/', train=True, download=True, transform=transform)
+        test_data = datasets.CIFAR10(root='data/', train=False, download=True, transform=transform)
+    else:
+        train_data = datasets.FashionMNIST(root='data/', train=True, download=True, transform=transform)
+        test_data = datasets.FashionMNIST(root='data/', train=False, download=True, transform=transform)
+    return train_data, test_data
+
+def sort_dataset(data):
+    # Get unique classes
+    unique_classes = list(data.class_to_idx.values())
+    # Dict that gets the data indices for each unique label
+    class_to_indices = {}
+    for cl in unique_classes:
+        class_to_indices[cl] = []
+    
+    for i in range(len(data)):
+        class_to_indices[data.targets[i]].append(i)
+
+    return class_to_indices
+
+def create_subset(data, class_to_indices, classes, path):
+    l = []
+    for cl in classes:
+        l.extend(class_to_indices[cl])
+    sub = Subset(dataset=data, indices=l)
+    print(len(sub.indices))
+    save(obj=sub, f=path)
+
+def save_subsets(subsets: dict, folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    for cl, sub in subsets.items():
+        print('Storing subset for class', cl)
+        save(obj=sub, f=f'{folder}/subset_{cl}.pth')
+
+
+if __name__ == '__main__':
+    if not os.path.exists('subset_data'):
+        os.makedirs('subset_data')
+    if not os.path.exists('models'):
+        os.makedirs('models')
+    if not os.path.exists('models/client'):
+        os.makedirs('models/client')
+    if not os.path.exists('models/server'):
+        os.makedirs('models/server')
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    # AlexNet tranformations
+    #transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    train_data, test_data = get_data(transform=transform, cifar_flag=True)
+    class_to_indices = sort_dataset(data=train_data)
+    create_subset(data=train_data, class_to_indices=class_to_indices, classes=[0], path='subset_data/sub_0.pth')
+    create_subset(data=train_data, class_to_indices=class_to_indices, classes=[1], path='subset_data/sub_1.pth')
+    create_subset(data=train_data, class_to_indices=class_to_indices, classes=[2], path='subset_data/sub_2.pth')
+    create_subset(data=train_data, class_to_indices=class_to_indices, classes=[3], path='subset_data/sub_3.pth')
--- src/main.py
+++ src/main.py
@@ -3,8 +3,18 @@ import torch
 import numpy as np
 from yaml import safe_load
 from tqdm import tqdm
+from torch.utils.data import DataLoader
 
 SEED = 32
+
+def create_dict(num_clients):
+    data_dict = {}
+    for i in range(num_clients):
+        data_dict[i] = {"weak_model": None, "strong_model": None, "server_model": None,
+                        "weak_optim": None, "strong_optim": None, "server_optim": None,
+                        "dataloader": None, "datasize": None}
+        
+    return data_dict
 
 def set_all_seeds(seed) -> None:
     np.random.seed(seed)
@@ -14,27 +24,26 @@ def set_all_seeds(seed) -> None:
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-def create_models_and_optims(num_clients, seed, lr):
-    weak_client_models, weak_optims  = [], []
-    collaborative_models, collaborative_optims = [], []
-    server_models, server_optims = [], []
+def load_data(subset_path, batch_size, shuffle):
+        subset = torch.load(f=subset_path)
+        return DataLoader(subset, batch_size=batch_size, shuffle=shuffle, num_workers=2, pin_memory=True), len(subset)
+
+def populate_dict(data_dict, seed, lr):
     print(f"[+] Instanciating models and optimizers")
-    for _ in tqdm(range(num_clients)):
+    for cid in tqdm(data_dict.keys()):
         torch.manual_seed(seed)
-        weak_model = ClientModel()
-        weak_optim = torch.optim.SGD(params=weak_model.parameters(), lr=lr)
+        data_dict[cid]["weak_model"] = ClientModel()
+        data_dict[cid]["weak_optim"] = torch.optim.SGD(params=data_dict[cid]["weak_model"].parameters(), lr=lr)
         torch.manual_seed(seed)
-        collab_model = CollaborativeModel()
-        collab_optim = torch.optim.SGD(params=collab_model.parameters(), lr=lr)
+        data_dict[cid]["strong_model"] = CollaborativeModel()
+        data_dict[cid]["strong_optim"] = torch.optim.SGD(params=data_dict[cid]["strong_model"].parameters(), lr=lr)
         torch.manual_seed(seed)
-        server_model = ServerModel()
-        server_optim = torch.optim.SGD(params=server_model.parameters(), lr=lr)
+        data_dict[cid]["server_model"] = ServerModel()
+        data_dict[cid]["server_optim"] = torch.optim.SGD(params=data_dict[cid]["server_model"].parameters(), lr=lr)
+        data_dict[cid]["dataloader"], data_dict[cid]["datasize"] = load_data(subset_path=f"/home/alex/Desktop/ASOEE/split_yannis/Collaboirative-SFL/subset_data/sub_{cid}.pth", batch_size=32, shuffle=True)
+    return data_dict
 
-        weak_client_models.append(weak_model), weak_optims.append(weak_optim)
-        collaborative_models.append(collab_model), collaborative_optims.append(collab_optim) 
-        server_models.append(server_model), server_optims.append(server_optim) 
 
-    return weak_client_models, weak_optims, collaborative_models, collaborative_optims, server_models, server_optim
 
 def read_config(file_path):
     with open(file_path, 'r') as file:
@@ -43,9 +52,12 @@ def read_config(file_path):
 
 def main():
     cfg = read_config("/home/alex/Desktop/ASOEE/split_yannis/Collaboirative-SFL/src/config.yaml")
-    print(cfg)
+    num_clients = cfg["weak_clients_num"] + cfg["strong_clients_num"]
+    data_dict = create_dict(num_clients=num_clients)
+    print(data_dict)
     set_all_seeds(cfg["seed"])
-    weak_client_models, weak_optims, collaborative_models, collaborative_optims, server_models, server_optims = create_models_and_optims(num_clients=cfg["weak_clients_num"] + cfg["strong_clients_num"], seed=cfg["seed"], lr=cfg["lr"])
+    data_dict = populate_dict(data_dict=data_dict, seed=cfg["seed"], lr=cfg["lr"])
+    print(data_dict)
 
 if __name__ == "__main__":
     main()
