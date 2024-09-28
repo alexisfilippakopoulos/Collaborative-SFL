36ac1408d00f5b8f125bcfd83a3a3cc41138b5c5
alex <alexisfilippakopoulos1@gmail.com>
2024-09-28 15:23:30 +0300

    Added loss tracking

--- src/main.py
+++ src/main.py
@@ -7,6 +7,10 @@ from torch.utils.data import DataLoader
 import threading
 from torch import nn
 from copy import deepcopy
+import warnings
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+
 
 SEED = 32
 
@@ -29,7 +33,7 @@ def set_all_seeds(seed) -> None:
 
 def load_data(subset_path, batch_size, shuffle):
         subset = torch.load(f=subset_path)
-        return DataLoader(subset, batch_size=batch_size, shuffle=shuffle, num_workers=2, pin_memory=True), len(subset), np.ceil(len(subset) / batch_size)
+        return DataLoader(subset, batch_size=batch_size, shuffle=shuffle, num_workers=2, pin_memory=True), int(len(subset)), int(np.ceil(len(subset) / batch_size))
 
 def populate_dict(data_dict, seed, lr):
     print(f"[+] Instanciating models and optimizers")
@@ -104,7 +108,7 @@ def client_aggregation(data_dict: dict[dict], device):
         metadata["strong_model"].load_state_dict(aggr_strong_model)
         metadata["weak_model"].load_state_dict(aggr_weak_model)
 
-    print("[+] Aggregated client-side models")
+    print("\t[+] Aggregated Client-Side Models")
     return data_dict    
 
 def server_aggregation(data_dict: dict[dict], device):
@@ -121,11 +125,13 @@ def server_aggregation(data_dict: dict[dict], device):
     for client, metadata in data_dict.items():
         metadata["server_model"].load_state_dict(aggr_server_model)
 
-    print("[+] Aggregated server-side models")
+    print("\t[+] Aggregated Server-Side Models")
     return data_dict
 
 def train_one_epoch(data_dict: dict[dict], criterion: nn, device: torch.device):
-    for _ in range(int(data_dict[0]["num_batches"])):
+    curr_clients_mean_loss = 0.
+    curr_server_mean_loss = 0.
+    for _ in range(data_dict[0]["num_batches"]):
             # List holding the client-side's outputs
             server_inputs = [None] * len(data_dict.keys())
             client_targets = [None] * len(data_dict.keys())
@@ -146,10 +152,10 @@ def train_one_epoch(data_dict: dict[dict], criterion: nn, device: torch.device):
             # compute and BP the strong and weak models based on all clients' mean loss for this batch
             mean_client_loss = torch.stack(losses).mean()
             mean_client_loss.backward()
+            curr_clients_mean_loss += mean_client_loss.item()
 
             for client, metadata in data_dict.items():
                 metadata['weak_optim'].step(), metadata['strong_optim'].step()
-            print(f"\tMean Client Loss: {mean_client_loss.item(): .5f}")
 
             losses = [None] * len(data_dict.keys())
 
@@ -166,23 +172,30 @@ def train_one_epoch(data_dict: dict[dict], criterion: nn, device: torch.device):
             # compute and BP the server models based on all models' mean loss for this batch
             mean_server_loss = torch.stack(losses).mean()
             mean_server_loss.backward()
-            print(f"\tMean Server Loss: {mean_server_loss.item(): .5f}")
+            curr_server_mean_loss += mean_server_loss.item()
 
             for client, metadata in data_dict.items():
                 metadata['server_optim'].step()
 
-    return data_dict
+    avg_client_mean_loss = round(curr_clients_mean_loss / data_dict[0]["num_batches"], 4)
+    avg_server_mean_loss = round(curr_server_mean_loss / data_dict[0]["num_batches"], 4)
+
+    return data_dict, avg_client_mean_loss, avg_server_mean_loss
 
 
 def train_both_parties(data_dict, epochs, criterion, device, fed_avg_freq):
-    for e in range(epochs):
+    for e in tqdm(range(epochs)):
         print(f"[+] Epoch: {e + 1}")
-
+        # create a dataloader iterator for current epoch
         for _, metadata in data_dict.items():
             metadata["data_iter"] = iter(metadata["dataloader"])
+        # train for one epoch
+        data_dict, avg_client_mean_loss, avg_server_mean_loss = train_one_epoch(data_dict=data_dict, criterion=criterion, device=device)
 
-        data_dict = train_one_epoch(data_dict=data_dict, criterion=criterion, device=device)
+        print(f"\t[+] Average Client-Side Mean Loss: {avg_client_mean_loss}")
+        print(f"\t[+] Average Server-Side Mean Loss: {avg_server_mean_loss}")
 
+        # if it's time to aggregate do
         if (e + 1) % fed_avg_freq == 0:
             data_dict = client_aggregation(data_dict=data_dict, device=device)
             data_dict = server_aggregation(data_dict=data_dict, device=device)
